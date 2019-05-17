bd8f6b0abdc174037bbaf227e94315f98b21ade0
14814 <1481454450@qq.com>
2019-05-17 17:21:19 +0800

    [注释] apply()与call()方法说明

--- packages/utils/mixins/emitter.js
+++ packages/utils/mixins/emitter.js
@@ -8,7 +8,8 @@ export default {
                 if (parent) name = parent.$options.name;
             }
             if (parent) parent.$emit.apply(parent, [eventName].concat(params))
-            //apply(this,[params])方法中的[eventName].concat(params)作为$emit的参数进行传递 即$emit('form-add',params)
+            //apply(thisArg,[arg1, arg2, ...])方法中的[eventName].concat(params)作为$emit的参数进行传递 即$emit('form-add',params)
+            // if (parent) parent.$emit.call(parent, eventName, params) //call(thisArg, arg1, arg2, ...)写法不同，作用相同
         }
     }
 }
